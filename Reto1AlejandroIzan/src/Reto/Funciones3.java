c2f233a22572a9e7bb146f508cb40516e5fa53ea
roterfile81 <1DAM@A11-PC06>
2025-11-17 12:19:14 +0100

    Parte Nabil Toumi

--- Reto1AlejandroIzan/src/Reto/Funciones3.java
+++ Reto1AlejandroIzan/src/Reto/Funciones3.java
@@ -1,5 +1,31 @@
 package Reto;
 
 public class Funciones3 {
-
+ public static int multiplicar(int a, int b) {
+ return a * b;
+ }
+ public static int dividir(int a, int b) {
+	 if (b<0) {
+		 return 0;
+	 }
+	 return a / b;
+	 }
+ public static boolean EsPositivo(int n) {
+	 if(n>=0) {
+		 return true;
+		 
+	 }else {
+		 return false;
+	 }
+ }
+ 
+ public static boolean EsNegativo(int n) {
+	 if(n<0) {
+		 return true;
+	 } else {
+		 return false;
+	 }
+	 
+ }
+ 
 }
