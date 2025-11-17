330ccea88443401ad51b702811786390ce923c60
1DAM <1DAM@A11-PC18>
2025-11-17 13:07:03 +0100

    hecho funciones 2

--- Reto1AlejandroIzan/src/Reto/Funciones2.java
+++ Reto1AlejandroIzan/src/Reto/Funciones2.java
@@ -1,5 +1,21 @@
 package Reto;
 
 public class Funciones2 {
-
+	
+	public static int sumar(int n1, int n2) {
+		return n1 + n2;
+	}
+	public static int restar(int n1, int n2) {
+		return n1 - n2;
+	}
+	public static boolean esPar(int n1) {
+		if (n1%2 == 0) {
+			return true;
+		}return false;
+	}
+	public static boolean esImpar(int n1) {
+		if (n1%2 != 0) {
+			return true;
+		}return false;
+}
 }
